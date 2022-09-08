5e1647aec8c8ed58038e49ac4f22a9d6d01f01fe
JonatasCopernico <jonatas.pimenta@copernico.com.br>
2022-09-08 12:27:44 -0300

    :rocket:

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Suspense } from 'react';
+import Model from './My_model';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className="helloDiv">
+        <h1>Hello, this is me in T-Pose!</h1>
+        <h2>just messing around with ThreeJS</h2>
+      </div>
+
+      <div className="center">
+        <Canvas
+          camera={{ position: [2, 0, 12.25], fov: 12 }}
+          style={{
+            backgroundColor: "#111a21",
+            width: "60vw",
+            height: "80vh",
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <ambientLight intensity={1.25} />
+          <ambientLight intensity={0.1} />
+          <directionalLight intensity={0.4} />
+          <Suspense fallback={null}>
+            <Model position={[0.025, -0.9, 0]} />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+      </div>
+    </>
   );
 }
 
